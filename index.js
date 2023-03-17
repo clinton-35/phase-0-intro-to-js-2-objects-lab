20f9b13ca16004bbb1e3065f6223c000b5761364
clinton <clinton.wambua@student.moringaschool.com>
2023-03-17 07:59:41 +0300

    assignment update

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee= {
+    name: 'clinton',
+    streetaddress:'241 Kasarani'
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+        const newemployee = {...employee};
+        newemployee[key]=value;
+        return newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key){
+    const employee3={...employee};
+    delete employee3[key];
+    return employee3;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
